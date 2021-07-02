34b991e22945ebc3e3d45cb0ad270a3222735cce
Sysprogs <sysprogs@sysprogs.com>
2021-07-02 11:41:09 -0700

    Forked the pic32mx FLASH driver to pic32mm and renamed the variables in it

--- src/flash/nor/drivers.c
+++ src/flash/nor/drivers.c
@@ -62,6 +62,7 @@ extern const struct flash_driver nrf51_flash;
 extern const struct flash_driver numicro_flash;
 extern const struct flash_driver ocl_flash;
 extern const struct flash_driver pic32mx_flash;
+extern const struct flash_driver pic32mm_flash;
 extern const struct flash_driver psoc4_flash;
 extern const struct flash_driver psoc5lp_flash;
 extern const struct flash_driver psoc5lp_eeprom_flash;
@@ -138,6 +139,7 @@ static const struct flash_driver * const flash_drivers[] = {
 	&numicro_flash,
 	&ocl_flash,
 	&pic32mx_flash,
+	&pic32mm_flash,
 	&psoc4_flash,
 	&psoc5lp_flash,
 	&psoc5lp_eeprom_flash,
